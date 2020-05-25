117762a0c3a43d8dec7049714d2cfa1647f93c7c
cristobal <candpalma@falabella.cl>
2020-05-25 11:38:40 -0400

    Revert "first commit"
    This reverts commit 87387ce775e2dc3d88b81efbcaa933812e93c4e3.

--- controllers/status.js
+++
@@ -1,7 +0,0 @@
-class Status {
-  static async status(req, res) {
-    res.status(200).json({ status: 'OK' });
-  }
-}
-
-module.exports = Status;
--- routers/index.js
+++
@@ -1,5 +0,0 @@
-const status = require('../controllers/status');
-
-module.exports = (app) => {
-  app.get('/status', status.status);
-};
