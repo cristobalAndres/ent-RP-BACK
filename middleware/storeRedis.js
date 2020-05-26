a9557b2d154cb1002cb56c88da63ac75d754a70d
cristobal <candpalma@falabella.cl>
2020-05-26 15:48:54 -0400

    add-redis

--- middleware/storeRedis.js
+++ middleware/storeRedis.js
@@ -1,6 +1,6 @@
 const redis = require("redis");
 const REDIS_PORT = process.env.REDIS_URL || 6379
-const client = redis.createClient("redis-18606.c17.us-east-1-4.ec2.cloud.redislabs.com:18606");
+const client = redis.createClient(18606, "redis-18606.c17.us-east-1-4.ec2.cloud.redislabs.com");
 
 
 async function Redis(req, res, next) {
